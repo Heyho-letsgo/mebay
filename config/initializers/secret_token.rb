65312cf7b56028a1e7820d3419359306749aff2c
Heyho-letsgo <abaudin@gmail.com>
2013-11-28 20:12:41 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mebay::Application.config.secret_key_base = 'e287f946f44d3c4ac0b57772df32297ba74c63819f100c6f37ac8edbedb9535137982ee0a4a5c42312e131d7df311eab1c740edec605323b375fe30c765365e4'
